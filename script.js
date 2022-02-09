634c5ab764dbe72230bdadd384accb7707da740b
i-webtest <stasbass80@gmail.com>
2022-02-09 11:44:10 +0300

    восстановил порядок книг

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+const books = document.querySelectorAll(".books");
+const book = document.querySelectorAll(".book");
+
+console.log(books);
+console.log(book);
+
+books[0].prepend(book[1], book[0], book[4], book[3], book[5]);
