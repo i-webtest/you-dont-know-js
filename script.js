02be6d3f19f672b129a12975f3ae1e8e802cf5c7
i-webtest <stasbass80@gmail.com>
2022-02-09 12:29:08 +0300

    заменил картинку заднего фона

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ console.log(books);
 console.log(book);
 
 books[0].prepend(book[1], book[0], book[4], book[3], book[5]);
+
+document.querySelector("body").style.backgroundImage =
+  "url(image/you-dont-know-js.jpg)";
